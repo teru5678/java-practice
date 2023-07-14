f31c8df558dc6a8bf6db1678a4e0e511e8839dce
teru5678 <toyoteru0719@gmail.com>
2023-07-14 18:41:13 +0900

    画面出力の命令

---
+++ practice2/Main6.java
@@ -0,0 +1,12 @@
+public class Main6 {
+    public static void main(String[] args) {
+        String name = "すがわら";
+        String message;
+        message = name + "さん、こんにちは";
+        System.out.println(message);
+        // 改行なしの画面出力の命令
+        System.out.print("私の名前は");
+        System.out.print(name);
+        System.out.print("です");
+    }
+}
