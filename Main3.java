ee65655a7ccfeff189abf7ecb641baf99aea00bb
teru5678 <toyoteru0719@gmail.com>
2023-07-14 17:28:42 +0900

    インクリメント・デクリメント演算子

---
+++ Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        // 演算子について
+        /* インクリメント・デクリメント演算子は基本単独で使用する */
+        int a;
+        a = 100;
+        a++;
+        System.out.println(a);
+    }
+}
