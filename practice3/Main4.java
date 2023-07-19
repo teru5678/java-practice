14037daf8fda28c2f39ee9329f788ef4e22ba54d
teru5678 <toyoteru0719@gmail.com>
2023-07-19 16:39:11 +0900

    ループ変数iの内容を表示する

---
+++ practice3/Main4.java
@@ -0,0 +1,7 @@
+public class Main4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("現在" + (i + 1) + "周目→");
+        }
+    }
+}
