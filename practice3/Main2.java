88a41e2fc2269be2f8ba9a604c010984468541b6
teru5678 <toyoteru0719@gmail.com>
2023-07-19 15:59:12 +0900

    繰り返し処理

---
+++ practice3/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        boolean doorClose = true;
+        while (doorClose == true) {
+            System.out.println("ノックする");
+            System.out.println("1分待つ");
+        }
+    }
+}
