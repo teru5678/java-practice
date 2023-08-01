9e4e20cc5af2003509c3dc792528768b2c84ec63
teru5678 <toyoteru0719@gmail.com>
2023-08-01 19:20:04 +0900

    練習問題

---
+++ workbook/Practice6.java
@@ -0,0 +1,12 @@
+public class Practice6 {
+    public static void main(String[] args) {
+        boolean tenki = true;
+        if (tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("DVDを見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
