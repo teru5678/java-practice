8551fb4bc34d6ae16679f0b57c8a087a96384623
teru5678 <toyoteru0719@gmail.com>
2023-08-05 19:15:17 +0900

    要素数の表示

---
+++ practice4/Main6.java
@@ -0,0 +1,9 @@
+package practice4;
+
+public class Main6 {
+    public static void main(String[] args) {
+        int[][] scores = { { 40, 50, 60 }, { 80, 60, 70 } };
+        System.out.println(scores.length);// 親配列の数を出力
+        System.out.println(scores[0].length);// 子配列の数を出力
+    }
+}
