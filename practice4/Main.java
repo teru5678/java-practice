2ef469310bebfbfe47d2a9229fa480cb7790db87
teru5678 <toyoteru0719@gmail.com>
2023-08-05 09:04:15 +0900

    配列の要素に値を代入方法

--- practice4/Main.java
+++ practice4/Main.java
@@ -2,7 +2,7 @@ package practice4;
 
 public class Main {
     public static void main(String[] args) {
-        int[] scores = new int[5];
+        int[] scores = new int[5];// 配列の長さを調べる
         int num = scores.length;
         System.out.println("要素の数：" + num);
     }
---
+++ practice4/Main2.java
@@ -0,0 +1,10 @@
+package practice4;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int[] scores;
+        scores = new int[5];
+        scores[1] = 30; // 二つ目の要素scoresに30を代入
+        System.out.println(scores[1]); // 二つ目の要素scores[1]の中身を表示
+    }
+}
