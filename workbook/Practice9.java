55765849ea29b51943aaafd913ce3772abeda771
teru5678 <toyoteru0719@gmail.com>
2023-08-07 21:23:46 +0900

    練習問題4-1

---
+++ workbook/Practice9.java
@@ -0,0 +1,10 @@
+package workbook;
+
+public class practice9 {
+    public static void main(String[] args) {
+        int[] points = new int[4];
+        double[] weights = new double[5];
+        boolean[] answers = new boolean[3];
+        String[] names = new String[3];
+    }
+}
