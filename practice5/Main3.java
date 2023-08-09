459f0b9d95fa7b02add1e5333b11400185e7eee7
teru5678 <toyoteru0719@gmail.com>
2023-08-09 18:37:12 +0900

    引数の例(渡す値が複数の場合)

---
+++ practice5/Main3.java
@@ -0,0 +1,13 @@
+package practice5;
+
+public class Main3 {
+    public static void main(String[] args) {
+        add(100, 20);
+        add(200, 50);
+    }
+
+    public static void add(int x, int y) {
+        int ans = x + y;
+        System.out.println(x + "+" + y + "=" + ans);
+    }
+}
