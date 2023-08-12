9bdee044777f99810ef803b595dca67bbe70301e
teru5678 <toyoteru0719@gmail.com>
2023-08-12 21:25:12 +0900

    練習問題

---
+++ workbook/work5/Practice.java
@@ -0,0 +1,20 @@
+package workbook.work5;
+
+public class Practice {
+
+    public static void introduceOneself() {
+        String name = "teruki";
+        int age = 25;
+        double height = 162.5;
+        char zodiac = '虎';
+        System.out.println("私の名前は" + name + "です");
+        System.out.println("年齢は" + age + "歳です");
+        System.out.println("私の身長は" + height + "cmです");
+        System.out.println("干支は" + zodiac + "年です");
+    }
+
+    public static void main(String[] args) {
+        introduceOneself();
+    }
+
+}
