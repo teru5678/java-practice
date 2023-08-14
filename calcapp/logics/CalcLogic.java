89440fc5865bd9bf56f476deb894a152d7b625e0
teru5678 <toyoteru0719@gmail.com>
2023-08-14 18:32:45 +0900

    インポートについて

--- calcapp/logics/CalcLogic.java
+++ calcapp/logics/CalcLogic.java
@@ -1,4 +1,3 @@
-package calcapp.logics;
 
 public class CalcLogic {
     public static int tasu(int a, int b) {
--- calcapp/main/Calc.java
+++ calcapp/main/Calc.java
@@ -1,11 +1,12 @@
-package calcapp.main;
+
+import calcapp.logics.*;
 
 public class Calc {
     public static void main(String[] args) {
         int a = 10;
         int b = 2;
-        int total = calcapp.logics.CalcLogic.tasu(a, b);
-        int delta = calcapp.logics.CalcLogic.hiku(a, b);
+        int total = CalcLogic.tasu(a, b);
+        int delta = CalcLogic.hiku(a, b);
         System.out.println("足すと" + total + "、引くと" + delta);
     }
 }
