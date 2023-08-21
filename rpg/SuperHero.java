1f8f99e983beb2d388f8bb3648fdcf154d965b47
teru5678 <toyoteru0719@gmail.com>
2023-08-21 13:53:00 +0900

    Heroクラスの継承

--- rpg/SuperHero.java
+++ rpg/SuperHero.java
@@ -1,19 +1,5 @@
-public class SuperHero {
-    String name = "ミナト";
-    int hp = 100;
+public class SuperHero extends Hero {
     boolean fflying;
-
-    // 戦う
-    public void attack(Matango m) {
-        System.out.println(this.name + "の攻撃！");
-        m.hp -= 5;
-        System.out.println("5ポイントのダメージを与えた!");
-    }
-
-    // 逃げる
-    public void run() {
-        System.out.println(this.name + "は逃げ出した!");
-    }
 
     // 飛ぶ
     public void fly() {
