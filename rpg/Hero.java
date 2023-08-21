d1793ed2ab759b9322f2b86082b47a3bc8f95bac
teru5678 <toyoteru0719@gmail.com>
2023-08-21 13:41:10 +0900

    戦うと逃げるを追加

--- rpg/Hero.java
+++ rpg/Hero.java
@@ -1,37 +1,18 @@
 package rpg;
 
 public class Hero {
-    String name;// 名前の宣言
-    int hp;// HPの宣言
+    String name = "ミナト";// 名前の宣言
+    int hp = "100";// HPの宣言
 
-    public void sleep() {
-        this.hp = 100;
-        System.out.println(this.name + "は、眠って回復した！");
+    // 戦う
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃!");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージを与えた!");
     }
 
-    public void sit(int sec) {
-        this.hp += sec;// 何秒座るか引数で受け取る
-        System.out.println(this.name + "は、" + sec + "秒座った！");
-        System.out.println("HPが" + sec + "ポイント回復した！");
-    }
-
-    public void slip() {
-        this.hp -= 5;
-        System.out.println(this.name + "は、転んだ！");
-        System.out.println("5のダメージ");
-    }
-
+    // 逃げる
     public void run() {
-        System.out.println(this.name + "は、逃げ出した！");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
+        System.out.println(this.name + "は逃げ出した！");
     }
-
-    public Hero(String name) {
-        this.hp = 100;
-        this.name = name;
-    }
-
-    public Hero(){
-        this("ダミー");
-    }
+}
