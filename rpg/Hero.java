d1f394750f95191f41735c6d445cd73ee0324962
teru5678 <toyoteru0719@gmail.com>
2023-08-26 18:52:53 +0900

    attackメソッドのオーバーライド

--- rpg/Hero.java
+++ rpg/Hero.java
@@ -1,18 +1,9 @@
 package rpg;
 
-public class Hero {
-    String name = "ミナト";// 名前の宣言
-    int hp = 100;// HPの宣言
-
-    // 戦う
+public class Hero extends Character {
     public void attack(Matango m) {
-        System.out.println(this.name + "の攻撃!");
-        m.hp -= 5;
-        System.out.println("5ポイントのダメージを与えた!");
-    }
-
-    // 逃げる
-    public void run() {
-        System.out.println(this.name + "は逃げ出した！");
+        System.out.println(this.name + "の攻撃");
+        System.out.println("敵に10ポイントのダメージを与えた!");
+        m.hp -= 10;
     }
 }
