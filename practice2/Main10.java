0a20a9b5d4fc1899ba5bc0487267bd3597aadace
teru5678 <toyoteru0719@gmail.com>
2023-09-04 23:55:47 +0900

    繰り返し出力

---
+++ practice2/Main10.java
@@ -0,0 +1,14 @@
+package practice2;
+
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Hello paiza");
+            scan.close();
+        }
+    }
+}
